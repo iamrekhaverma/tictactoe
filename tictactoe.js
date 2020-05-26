de434f3452c3cfeeeb480ba9081b6f643d5e27ef
rekhaverma <rekha.verma@quovantis.com>
2020-05-26 09:42:25 +0530

    Initial commit for tictac toe

---
+++ tictactoe.js
@@ -0,0 +1,102 @@
+var previousTurn = "X";
+function checkIfMatrixIsFilled(gameMatrix) {
+  let filled = true;
+  for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++) {
+      if(gameMatrix[i][j]!== "X" || gameMatrix[i][j]!== "Y") {
+        filled = false;
+        break;
+      }
+    }
+  }
+  return filled;
+}
+
+function initialize() {
+  var gameMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  return gameMatrix;
+}
+
+let elements = document.getElementsByClassName("grid-item");
+let gameMatrix = initialize();
+var handleMatrixColumnClick = function() {
+  if(previousTurn == "X") {
+    previousTurn = "Y";
+    currentTurn = "X";
+ } else {
+   previousTurn = "X";
+   currentTurn = "Y"
+ }
+  let row = this.getAttribute("row");
+  let col = this.getAttribute("col");
+    this.innerText = currentTurn;
+    gameMatrix[row][col]= currentTurn;
+    let over = checkForCondition(gameMatrix);
+    if(over) {
+      alert("Won Player" + currentTurn);
+      initialize();
+      previousTurn = "X";
+    } else {
+      let filled = checkIfMatrixIsFilled(gameMatrix);
+      if(filled) {
+        alert("game over");
+        initialize();
+        previousTurn = "X";
+      }
+    }
+};
+
+function checkForCondition(gameMatrix) {
+  console.log("gameMatrix",gameMatrix)
+  console.log("gameMatrix[0][0]",gameMatrix[0][0], gameMatrix[1][1], gameMatrix[2][2])
+  let win = false;
+    if ((gameMatrix[0][0] == gameMatrix[1][1]) && (gameMatrix[1][1] == gameMatrix[2][2]))
+    {
+        win = true;
+        return win;
+    }
+    if ((gameMatrix[2][0] == gameMatrix[1][1]) && (gameMatrix[1][1] == gameMatrix[0][2]))
+    {
+        win = true;
+        return win;
+    }
+
+    if (checkColumn(gameMatrix) || checkRow (gameMatrix))
+    {
+        win = true;
+        return win;
+    }
+    console.log("win",win);
+    return win;
+}
+
+function checkRow()
+{
+    for (let i = 0; i<3; i++)
+    {
+        if ((gameMatrix[i][0] == gameMatrix[i][1]) && (gameMatrix[i][1] == gameMatrix[i][2]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+function checkColumn ()
+{
+    for (let i = 0; i<3; i++)
+    {
+        if ((gameMatrix[0][i] == gameMatrix[1][i]) && (gameMatrix[1][i] == gameMatrix[2][i]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', handleMatrixColumnClick, false);
+}
